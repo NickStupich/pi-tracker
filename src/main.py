005ff6e363d2741d8d6f4a0b4771c167220df538
Nick Stupich <nick.stupich@gmail.com>
2020-05-02 14:13:54 -0700

    pin changes to other spi port

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     gear_ratio = 128.
     steps_per_rotation = 400.
     base_steps_per_second_ra = steps_per_rotation * gear_ratio / seconds_per_rotation  
-    motor_ra = motor_control.MotorControl(bus=0, cs_pin = 0, slave_pin=26, reset_pin = 3, 
+    motor_ra = motor_control.MotorControl(bus=1, cs_pin = 0, slave_pin=26, reset_pin = 3, 
             speed_adjustment_msg = messages.CMD_SET_SPEED_ADJUSTMENT_RA,
             base_steps_per_second = base_steps_per_second_ra,
             default_speed = 1,
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     gear_ratio = (99 + 1044./ 2057.) * 27 * 84 / 52.
     steps_per_rotation = 200.
     base_steps_per_second_dec = steps_per_rotation * gear_ratio / seconds_per_rotation 
-    motor_dec = motor_control.MotorControl(bus=0, cs_pin = 0, slave_pin=25, reset_pin = 3, 
+    motor_dec = motor_control.MotorControl(bus=1, cs_pin = 0, slave_pin=25, reset_pin = 3, 
             speed_adjustment_msg = messages.CMD_SET_SPEED_ADJUSTMENT_DEC,
             base_steps_per_second = base_steps_per_second_dec,
             default_speed = 0,
